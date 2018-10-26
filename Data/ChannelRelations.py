640dc75fb6b5aa6f6ec1258ad5b2239e336bc191
Mark Ledwich <mark@ledwich.com.au>
2018-10-27 08:43:30 +1000

    - include ID in relations for vis

--- Data/ChannelRelations.py
+++ Data/ChannelRelations.py
@@ -8,7 +8,7 @@ allChannels = p.read_json('.\\2.Analysis\\channels.json')
 allChannels[['id','title','subCount']].to_csv('.\\3.Vis\\Channels.csv')
 
 # save to csv of relations
-relations = table.groupby(['FromChannelTitle', 'ChannelTitle', 'DistanceFromSeed']).size()
+relations = table.groupby(['FromChannelTitle', 'ChannelTitle', 'FromChannelId', 'ChannelId', 'DistanceFromSeed']).size().to_frame('Size')
 relations.to_csv('.\\3.Vis\\ChannelRelations.csv', header=True)
 
 
