34d14a5d6a295fea25fc919472582733bc248123
Mark Ledwich <mark@ledwich.com.au>
2018-11-29 14:45:17 +1000

    build pipelines

--- App/buildYtCli.ps1
+++ App/buildYtCli.ps1
@@ -1,10 +1,10 @@
 $t = "ytnetworks.azurecr.io/ytnetworks:latest"
+docker build -t $t .
+
 #$cs = [Environment]::GetEnvironmentVariable("YtNetworks_AzureStorageCs", "User")
 
 #dotnet pack -o ./publish
 #dotnet publish -o ./publish -c Release --runtime linux-x64 --self-contained
-docker build -t $t .
-
 #docker tag ytnetworks/ytnetworks ytnetworks.azurecr.io/ytnetworks:latest
 #docker run -it -e YtNetworks_AzureStorageCs=$cs $t
 #docker push $t
