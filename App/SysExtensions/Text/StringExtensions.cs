6e0fe75796e81e1389d1b9ad1132ca6e36cf5a0f
Mark Ledwich <mark@ledwich.com.au>
2019-11-05 12:43:07 +1000

    Fix some issues with the v0 stored records

--- App/SysExtensions/Text/StringExtensions.cs
+++ App/SysExtensions/Text/StringExtensions.cs
@@ -112,6 +112,8 @@ namespace SysExtensions.Text {
 
     public static string StripNonDigit(this string s) => Regex.Replace(s, "\\D", "");
 
+    public static long ParseInt(this string s) => int.Parse(s);
+
     public static long ParseLong(this string s) {
       const NumberStyles styles = NumberStyles.AllowThousands;
       var format = NumberFormatInfo.InvariantInfo;
