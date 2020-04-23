0fc8a04b2ad322f8340b6f68df6a4535b1749088
Mark Ledwich <mark@ledwich.com.au>
2020-04-24 09:18:49 +1000

    search cleanup, tableau updates

--- Site/src/pages/search.tsx
+++ Site/src/pages/search.tsx
@@ -1,11 +1,9 @@
 import * as React from "react"
-import { Router, RouteComponentProps as CP } from "@reach/router"
-import { Video } from "../components/VideoPage"
 import { VideoSearch } from '../components/VideoSearch'
 import { MainLayout } from '../components/MainLayout'
 
 const SearchPage = () => (
-    <MainLayout path="video">
+    <MainLayout>
         <VideoSearch />
     </MainLayout>
 )
