fa03577003a45f211952d4662a3b4ed973782256
Mark Ledwich <mark@ledwich.com.au>
2020-05-26 18:31:15 +1000

    logging, test videos, video unavailable (#14)

--- UserScrape/app.py
+++ UserScrape/app.py
@@ -1,61 +1,76 @@
 import os
 from cfg import load_cfg
 from crawler import Crawler
-from selenium.common.exceptions import NoSuchElementException
-from data import load_all_seeds, seeds_for_user
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from data import load_seed_videos, load_test_videos
 import asyncio
 import argparse
 from typing import List
 from store import BlobStore
 from azure.storage.blob import PublicAccess
 from discord_bot import DiscordBot
+import logging
+from log import configure_log
+import time
+import seqlog
+from cfg import UserCfg
+import shortuuid
 
 
-async def experiment(initialization: bool, accounts: List[int]):
+async def experiment(initialization: bool, accounts: List[str]):
+
+    # cfg
     cfg = load_cfg()
-    video_seeds_df = load_all_seeds()
 
-    cfg = load_cfg()
-    if initialization:
-        video_seeds_df = load_all_seeds()
-    else:
-        # todo: function that returns only 5 sampled videos
-        video_seeds_df = load_all_seeds()[0:5]
+    experiment_id = shortuuid.random(8)
+    configure_log(cfg.seqUrl, experiment_id=experiment_id)
 
-    repetitions = 2  # 100
-    # todo: this list of videos needs to be sampled
-    test_videos = ['uo9dAIQR3g8', 'CH50zuS8DD0', '9_R3_CThc38']
+    log = logging.getLogger('seq')
+    users: List[UserCfg] = [u for u in cfg.users if accounts == None or u.ideology in accounts]
 
+    log.info("Experiment started. Init={initialization}, Accounts={accounts}",
+             initialization=initialization, accounts='|'.join([user.email for user in users]))
+
+    videos_to_test = load_test_videos()
+    videos_to_seed = load_seed_videos(50 if initialization else 5)
+
+    # env
     store = BlobStore(cfg.data_storage_cs, 'userscrape')
     store.ensure_exits(PublicAccess.Container)
-
     bot = DiscordBot(cfg.discord)
     await bot.start_in_backround()
 
     try:
-        for user in [cfg.users[i] for i in accounts]:
-            print(f'scraping for user {user.email}')
-            crawler = Crawler(store, bot, user, cfg.headless)
-            # crawler.test_ip()
+        for user in users:
+            log.info('{email} - started scraping', email=user.email)
+            start = time.time()
 
-            user_seed_videos = seeds_for_user(user, video_seeds_df)
+            crawler = Crawler(store, bot, user, cfg.headless, log)
+            user_seed_videos = videos_to_seed[user.ideology]
+            user_seed_video_ids: List[str] = [video.video_id for video in user_seed_videos]
 
             try:
-                # crawler.load_home_and_login()
-                await crawler.login()
+                await crawler.load_home_and_login()
+                log.info("{email} - logged in", email=user.email)
                 crawler.delete_history()
-                for repetition in range(1, repetitions):
-                    await crawler.watch_videos([video.video_id for video in user_seed_videos[0:5]])
-                    crawler.scan_feed()
-                    # 115 test videos
-                    for video in test_videos:
-                        crawler.get_recommendations_for_video(video)
-                        crawler.delete_last_video_from_history(video)
-                    crawler.delete_history()
-                    crawler.update_trial()
-                    crawler.shutdown()
-            except NoSuchElementException as e:
-                print(f'Not able to find a required element {e.msg}. {user.email}')
+
+                log.info("{email} - seeding with {videos}", email=user.email, videos='|'.join(user_seed_video_ids))
+                await crawler.watch_videos(user_seed_video_ids)
+                crawler.scan_feed()
+
+                log.info("{email} - collecting recommendations for {video_number} videos",
+                         email=user.email, videos=len(videos_to_test))
+                for video in videos_to_test:
+                    crawler.get_recommendations_for_video(video.video_id)
+                    crawler.delete_last_video_from_history(video.video_id)
+                crawler.delete_history()
+                crawler.update_trial()
+                crawler.shutdown()
+
+                log.info("{email} - complete in {duration}s", email=user.email, duration=time.time() - start)
+
+            except BaseException:
+                log.error('{email} - unhandled error. aborting scraping for this user', email=user.email, exc_info=1)
             finally:
                 crawler.shutdown()
     finally:
@@ -63,34 +78,31 @@ async def experiment(initialization: bool, accounts: List[int]):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Start one iteration of the experiment. If you run this for the first time \
-        set --init. If you do not want to run the experiment with all accounts, specifiy the list with --accounts')
+        set --init. If you do not want to run the experiment with all accounts, specify the list with --accounts')
     parser.add_argument("--init", "-i",
                         help="Provide this argument if the experiment shall start from the beginning. Each account will start with a clear history and will watch 50 initial videos",
                         action='store_true')
     parser.add_argument("--accounts", "-a",
-                        help="A list of numbers for which the corresponding accounts will be included in this run of the experiment. \
+                        help="A | separeted list users for which the corresponding accounts will be included in this run of the experiment. \
      Possible options are \n \
-        0. White Identitarian \n \
-        1. MRA \n \
-        2. Conspiracy \n \
-        3. Libertarian \n \
-        4. Provocative Anti-SJW \n \
-        5. Anti-SJW \n \
-        6. Socialist \n \
-        7. Religious Conservative \n \
-        8. Social Justice \n \
-        9. Center/Left MSM \n \
-        10. Partisan Left \n \
-        11. Partisan Right \n \
-        12. Anti-Theist \n \
-        13. Uniform (An equal number of videos from each class) \n \
-        14. Videos are watched proportional to their Viewcount \n \
-        15. Non-Political (Non-political videos) \n \
-        16. A fresh account without viewing history ",
-                        default=list(range(0, 17)),
-                        nargs="+", type=int,
-                        choices=list(range(0, 17))
-                        )
+        White Identitarian \n \
+        MRA \n \
+        Conspiracy \n \
+        Libertarian \n \
+        Provocative Anti-SJW \n \
+        Anti-SJW \n \
+        Socialist \n \
+        Religious Conservative \n \
+        Social Justice \n \
+        Center/Left MSM \n \
+        Partisan Left \n \
+        Partisan Right \n \
+        Anti-Theist \n \
+        Uniform (An equal number of videos from each class) \n \
+        Proportional (Videos are watched proportional to their Viewcount) \n \
+        Non-Political (Non-political videos) \n \
+        Fresh (a fresh account without viewing history)",
+                        default=None)
     args = parser.parse_args()
 
-    asyncio.run(experiment(args.init, args.accounts))
+    asyncio.run(experiment(args.init, args.accounts.split('|') if args.accounts else None))
---
+++ UserScrape/log.py
@@ -0,0 +1,36 @@
+import seqlog
+import logging
+import json
+
+
+def configure_log(url: str, env='Debug', experiment_id=None):
+    seqlog.configure_from_dict({
+        'version': 1,
+        'disable_existing_loggers': True,
+        'root': {
+            'level': 'WARN',
+            'handlers': ['console']
+        },
+        'loggers': {
+            'seq': {
+                'level': 'INFO',
+                'handlers': ['seq']
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'seqlog.structured_logging.ConsoleStructuredLogHandler'
+            },
+            'seq': {
+                'class': 'seqlog.structured_logging.SeqLogHandler',
+                'server_url': url,
+                'batch_size': 1
+            }
+        }
+    })
+
+    seqlog.set_global_log_properties(
+        app="UserScrape",
+        env=env,
+        experiment=experiment_id
+    )
